479fc0d63c0099743e8ead357cfbbdcbbcc7f055
Michael Wenger <michael.wenger@arcade.ch>
2018-04-03 07:11:31 +0200

    DefaultIISSite Option added

--- DSCResources/VE_SFStoreWebReceiver/VE_SFStoreWebReceiver.ps1
+++ DSCResources/VE_SFStoreWebReceiver/VE_SFStoreWebReceiver.ps1
@@ -18,6 +18,9 @@ function Get-TargetResource {
         [Parameter()]
         [System.Boolean] $ClassicReceiver,
 
+        [Parameter()]
+        [System.Boolean] $DefaultIISSite,
+
         [Parameter()] [ValidateSet('Present','Absent')]
         [System.String] $Ensure = 'Present'
     )
@@ -28,6 +31,7 @@ function Get-TargetResource {
             VirtualPath = $webReceiver.VirtualPath
             StoreVirtualPath = $webReceiver.StoreServiceVirtualPath;
             ClassicReceiver = $webReceiver.WebReceiver.ClassicReceiverExperience;
+            DefaultIISSite = $webReceiver.DefaultIISSite;
             SiteId = $webReceiver.SiteId;
             Ensure = if ($webReceiver) { 'Present' } else { 'Absent' };
         }
@@ -53,6 +57,9 @@ function Test-TargetResource {
         [Parameter()]
         [System.Boolean] $ClassicReceiver,
 
+        [Parameter()]
+        [System.Boolean] $DefaultIISSite,
+
         [Parameter()] [ValidateSet('Present','Absent')]
         [System.String] $Ensure = 'Present'
     )
@@ -67,7 +74,7 @@ function Test-TargetResource {
 
         ## Only check all remaing properties if we're setting
         if ($Ensure -eq 'Present') {
-            $properties = @('VirtualPath','StoreServiceVirtualPath','SiteId','ClassicReceiver');
+            $properties = @('VirtualPath','StoreServiceVirtualPath','SiteId','ClassicReceiver','DefaultIISSite');
             foreach ($property in $properties) {
                 if ($PSBoundParameters.ContainsKey($property)) {
                     $propertyValue = (Get-Variable -Name $property).Value;
@@ -107,6 +114,9 @@ function Set-TargetResource {
         [Parameter()]
         [System.Boolean] $ClassicReceiver,
 
+        [Parameter()]
+        [System.Boolean] $DefaultIISSite,
+
         [Parameter()] [ValidateSet('Present','Absent')]
         [System.String] $Ensure = 'Present'
     )
@@ -123,10 +133,19 @@ function Set-TargetResource {
         }
         elseif ($Ensure -eq 'Present') {
             if ($webReceiver) {
+                $addSTFWebReceiverServiceParams = @{
+                    'WebReceiverService' = $webReceiver
+                }
                 if ($ClassicReceiver -ne $webReceiver.WebReceiver.ClassicReceiverExperience) {
                     Write-Verbose ($localizedData.UpdatingResourceProperty -f 'ClassicReceiver', $ClassicReceiver);
-                    [ref] $null = Set-STFWebReceiverService -WebReceiverService $webReceiver -ClassicReceiverExperience $ClassicReceiver;
+                   $addSTFWebReceiverServiceParams['ClassicReceiverExperience'] = $ClassicReceiver
                 }
+                if ($DefaultIISSite -ne $webReceiver.DefaultIISSite) {
+                    Write-Verbose ($localizedData.UpdatingResourceProperty -f 'DefaultIISSite', $DefaultIISSite);
+                   $addSTFWebReceiverServiceParams['DefaultIISSite'] = $DefaultIISSite
+                }
+                [ref] $null = Set-STFWebReceiverService @addSTFWebReceiverServiceParams;
+
             }
             else {
                 Write-Verbose -Message ($localizedData.AddingWebReceiver -f $VirtualPath);
@@ -137,6 +156,9 @@ function Set-TargetResource {
                 }
                 if ($PSBoundParameters.ContainsKey('SiteId')) {
                     $addSTFWebReceiverServiceParams['SiteId'] = $SiteId;
+                }
+                if ($PSBoundParameters.ContainsKey('DefaultIISSite')) {
+                   $addSTFWebReceiverServiceParams['DefaultIISSite'] = $DefaultIISSite
                 }
                 [ref] $null = Add-STFWebReceiverService @addSTFWebReceiverServiceParams;
             }
